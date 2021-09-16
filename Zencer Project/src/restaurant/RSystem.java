da7b447e304ae6d42768d38ea732681fd1ddec6a
Wankhadepriya <89521221+Wankhadepriya@users.noreply.github.com>
2021-09-16 18:38:23 +0530

    Add files via upload
    Final commit

--- Zencer Project/src/restaurant/RSystem.java
+++ Zencer Project/src/restaurant/RSystem.java
@@ -2,11 +2,13 @@ package restaurant;
 
 import java.util.*;
 
-public class RSystem extends Implementation {
+public class RSystem extends Implementation 
+{
 
 	public static final Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception
+	{
 		// TODO Auto-generated method stub
 		RSystem rest = new RSystem();
 		Implementation imp = new Implementation();
@@ -14,9 +16,9 @@ public class RSystem extends Implementation {
 		boolean flag = true;
 
 		while (flag) {
-			System.out.println("Enter one choice from following...");
+			System.out.println("---------Enter one choice from following-------\n");
 			System.out.println("1. Search reastuarant by name");
-			System.out.println("2. Show details of reastuarant");
+			System.out.println("2. For display all details of reastuarants");
 			System.out.println("3. Add new reastuarant");
 			System.out.println("4. Update details of an restuarant");
 			System.out.println("5. Delete reastuarant");
@@ -26,36 +28,37 @@ public class RSystem extends Implementation {
 
 			int userInput = sc.nextInt();
 
-			switch (userInput) {
+			switch (userInput) 
+			{
 
-			case 1:
-
-				imp.searchRestuarant();
-				break;
-			case 2:
-				imp.displayDetailsOfRestarant();
-
-				break;
-			case 3:
-				imp.addRestuarant();
-				break;
-			case 4:
-				rest.updateDetailsOfRestuarent();
-				break;
-			case 5:
-				rest.deleteDetailsOfRestuarant();
-				break;
-			case 6:
-				rest.activateDeactivate();
-				break;
-			case 7:
-				flag = false;
-				System.out.println("Bye...Thank You");
-				break;
-
-			default:
-				System.out.println("enter proper choise");
-				break;
+				case 1:
+	
+					imp.searchRestuarant();
+					break;
+				case 2:
+					imp.displayDetailsOfRestarant();
+	
+					break;
+				case 3:
+					imp.addRestuarant();
+					break;
+				case 4:
+					rest.updateDetailsOfRestuarent();
+					break;
+				case 5:
+					rest.deleteDetailsOfRestuarant();
+					break;
+				case 6:
+					rest.activateDeactivate();
+					break;
+				case 7:
+					flag = false;
+					System.out.println("Bye...Thank You");
+					break;
+	
+				default:
+					System.out.println("Enter proper choise");
+					break;
 
 			}
 
